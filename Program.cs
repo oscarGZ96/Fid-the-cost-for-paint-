7fc0330cadc2cb76a69b0b35073ec4ad91d55e3b
oscarGZ96 <52112142+oscarGZ96@users.noreply.github.com>
2019-06-23 23:10:19 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 0;
+            int width = 0;
+            Console.WriteLine("please enter the length of the room");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the width of the room");
+            width = Convert.ToInt32(Console.ReadLine());
+            int newEstimate = Estimate(length, width);
+            Console.WriteLine("your total cost will be $" + newEstimate);
+            Console.ReadLine();
+        }
+
+        static int Estimate (int length, int width)
+        {
+            int cost = 0;
+
+            int newWidth = width * 2;
+            int newLength = length * 2;
+            int TotalSquareFoot = (newLength + newWidth) * 9;
+            cost = TotalSquareFoot * 6;
+            Console.WriteLine(cost);
+
+            return cost;
+        }
+    }
+}
